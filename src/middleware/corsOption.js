165336a26c17d0de2d5b05616256f11560d3f96d
kimi.casamina <kimi.casamina@gmail.com>
2024-12-06 18:16:45 +0800

    updated cors option

--- src/middleware/corsOption.js
+++ src/middleware/corsOption.js
@@ -1,20 +1,13 @@
-const WHITELIST = [
-  "https://www.google.com",
-  "http://127.0.0.1:9090",
-  "http://127.0.0.1:9000",
-  "http://127.0.0.1:8000",
-  "http://127.0.0.1:8080",
-  "http://localhost:5173",
-];
-const corsOption = {
-  origin: (origin, callback) => {
-    if (WHITELIST.indexOf(origin) !== -1 || !origin) {
-      console.log("CORS ORIGIN: ", origin);
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+import dotenv from "dotenv";
+dotenv.config();
+
+const corsOptions = {
+  origin:
+    process.env.NODE_ENV === "production"
+      ? process.env.PASSTHESALT_CLIENT_URL
+      : "http://localhost:5173", // Vite default port for development
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 export default corsOption;
